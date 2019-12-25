dc4312272617643dac42df1bf5fb278a77fd89af
Kim Su Hyeon <kshvv975@gmail.com>
2019-12-25 18:36:29 +0900

    Complete AddcardActivity Get Image from Gallery

--- app/src/main/java/com/tistory/comfy91/excuseme_android/AddCardActivity.kt
+++ app/src/main/java/com/tistory/comfy91/excuseme_android/AddCardActivity.kt
@@ -1,13 +1,134 @@
 package com.tistory.comfy91.excuseme_android
 
+import android.Manifest
+import android.app.Activity
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_add_card.*
 
 class AddCardActivity : AppCompatActivity() {
+    private val TAG = javaClass.name
+    private lateinit var dialogBuilder: AlertDialog.Builder
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_card)
+
+
+        uiInit()
+
+        checkPermission()
     } // end onCrate()
+
+    companion object{
+        // image pick code
+        private val IMAGE_PICK_CODE = 1000
+        // permission code
+        private val PERMISSION_CODE = 1111
+
+    }
+
+
+    private fun uiInit(){
+
+        dialogBuilder = AlertDialog.Builder(this)
+
+        // 이미지 가져오기 리스너 설정
+        imgAddcardCardImg.setOnClickListener{
+            getImageFromAlbum()
+        }
+
+        btnAddcardRecord.setOnClickListener {
+
+        }
+
+
+    }
+
+    private fun getImageFromAlbum(){
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent,IMAGE_PICK_CODE )
+    }
+
+    private fun checkPermission(){
+        // 권한 동의 체크
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+            || ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+        {
+            // 권한 동의 안된 경우
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE))
+            {
+
+                    // 사용자 권한 동의 팝업 띄움
+                    dialogBuilder.setMessage("권한이 거부 되었습니다. 직접 권한을 허용하세요.")
+                        .setPositiveButton("상세", DialogInterface.OnClickListener{dialogInterface, i ->
+                            intent = Intent()
+                                .apply {
+                                    action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                                    data = Uri.fromParts("package", packageName, null)
+                                    startActivity((intent))
+
+                                }
+
+                        })
+                        .setNegativeButton("취소", DialogInterface.OnClickListener { dialogInterface, i ->
+                            finish()
+                        })
+
+            }
+            else{
+                // 사용자 권한 동의 팝업 표시
+                ActivityCompat.requestPermissions(this,
+                    arrayOf<String>(
+                    (Manifest.permission.READ_EXTERNAL_STORAGE)
+                    ),
+                    1111)
+
+            } // end if...else
+
+        }
+    } // end checkPermission()
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when(requestCode){
+            PERMISSION_CODE ->{
+                for(i in 0 until grantResults.size){
+                    if(grantResults[i] < 0){
+                        Toast.makeText(applicationContext, "해당 권한을 활성화 하셔야 합니다.", Toast.LENGTH_LONG).show()
+                        break
+                    }
+                }
+
+            } // end 1111
+        }
+    } // end onRequestPermissionREsult()
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.d(TAG, "(requestCode : $requestCode) (resulrCode: $resultCode)")
+
+        if(resultCode == Activity.RESULT_OK && requestCode== IMAGE_PICK_CODE){
+            // 정상적으로 이미지를 가져온 경우
+            Log.d(TAG, "Success Get Image from Gallery")
+            imgAddcardCardImg.setImageURI(data?.data)
+        }
+    }
 
 } // end class
