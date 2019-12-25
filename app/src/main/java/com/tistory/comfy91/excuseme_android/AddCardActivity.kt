00c4a4d0e3b7f0e03ee85135f60712272893170f
Kim Su Hyeon <kshvv975@gmail.com>
2019-12-25 15:32:49 +0900

    Complete AddCardActivity Layout

---
+++ app/src/main/java/com/tistory/comfy91/excuseme_android/AddCardActivity.kt
@@ -0,0 +1,13 @@
+package com.tistory.comfy91.excuseme_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddCardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_card)
+    } // end onCrate()
+
+} // end class
