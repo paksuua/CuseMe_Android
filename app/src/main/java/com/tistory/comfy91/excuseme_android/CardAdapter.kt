0d186f7586a3f7eb0dc2719cbd118be75b637e05
JIWONKIM <qhsh866@gmail.com>
2019-12-27 15:11:30 +0900

    중복 터치 처리

--- app/src/main/java/com/tistory/comfy91/excuseme_android/CardAdapter.kt
+++ app/src/main/java/com/tistory/comfy91/excuseme_android/CardAdapter.kt
@@ -2,25 +2,68 @@ package com.tistory.comfy91.excuseme_android
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.recyclerview.widget.RecyclerView
 
 
-class CardAdapter(private val arrData: ArrayList<ItemCard>) :
-    RecyclerView.Adapter<CardViewHolder>() {
+class CardAdapter(private val ItemCardList: ArrayList<ItemCard>) :
+    RecyclerView.Adapter<CardViewHolder>(), Filterable {
 
-    var data = arrData
+
+    var data = arrayListOf<ItemCard>()
+
+    //필터 전 리스트 = 전체 리스트
+    val unFilteredList = ItemCardList
+
+    //필터 중 리스트
+    var filteredList = ItemCardList
+
+    //var data = arrData
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewHolder {
         val view = LayoutInflater.from(parent!!.context).inflate(R.layout.item_card, parent, false)
         return CardViewHolder(view);
     }
 
-    override fun getItemCount(): Int {
-        return data.size
-    }
+    //fun getItemCount(data): Int {
+    //return data.size
+    //}
 
     override fun onBindViewHolder(holder: CardViewHolder, position: Int) {
         holder.bind(data.get(position))
     }
+
+
+    override fun getItemCount(): Int {
+        return filteredList.size
+    }
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val charString = constraint.toString()
+                filteredList = if (charString.isEmpty()) { //⑶
+                    unFilteredList
+                } else {
+                    var filteringList = ArrayList<ItemCard>()
+                    for (item in unFilteredList) {
+                        if (item.title == charString) filteringList.add(item)
+                    }
+                    filteringList
+                }
+                val filterResults = FilterResults()
+                filterResults.values = filteredList
+                return filterResults
+            }
+
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                filteredList =
+                    results?.values as ArrayList<ItemCard>
+                notifyDataSetChanged()
+            }
+        }
+    }
+
 
 }
--- app/src/main/java/com/tistory/comfy91/excuseme_android/CardViewHolder.kt
+++ app/src/main/java/com/tistory/comfy91/excuseme_android/CardViewHolder.kt
@@ -1,5 +1,6 @@
 package com.tistory.comfy91.excuseme_android
 
+
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
@@ -7,12 +8,14 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
 
-class CardViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+class CardViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val imgCard: ImageView = itemView.findViewById(R.id.imgCard)
     private val tvCard: TextView = itemView.findViewById(R.id.tvCardTitle)
 
-    fun bind(data: ItemCard){
+    fun bind(data: ItemCard) {
         Glide.with(itemView).load(data.imageUrl).into(imgCard)
         tvCard.text = data.title
     }
+
 }
+
