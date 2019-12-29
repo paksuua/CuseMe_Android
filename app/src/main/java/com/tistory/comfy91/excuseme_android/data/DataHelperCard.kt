86f13e47db8648bcc7c4ea07d7e27f34a5176a29
paksuua <paksuua@naver.com>
2019-12-29 16:21:00 +0900

    1229_1620 HelperActivity View with Bottom bar v2

---
+++ app/src/main/java/com/tistory/comfy91/excuseme_android/data/DataHelperCard.kt
@@ -0,0 +1,8 @@
+package com.tistory.comfy91.excuseme_android.data
+
+data class DataHelperCard(
+    val imageUrl: String,
+    val title: String,
+    var visibility: Boolean,
+    val desc: String
+)
