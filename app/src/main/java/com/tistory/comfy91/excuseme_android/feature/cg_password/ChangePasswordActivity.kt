f8c6681e948174d0cb1b43e6da96c9b87cffb5e2
kshvv975@gmail.com <RoadtoS7@users.noreply.github.com>
2020-01-03 04:30:26 +0900

    ChangePasswordActivity Server Connect

--- app/src/main/java/com/tistory/comfy91/excuseme_android/feature/cg_password/ChangePasswordActivity.kt
+++ app/src/main/java/com/tistory/comfy91/excuseme_android/feature/cg_password/ChangePasswordActivity.kt
@@ -8,37 +8,56 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.widget.doOnTextChanged
 import com.tistory.comfy91.excuseme_android.R
+import com.tistory.comfy91.excuseme_android.data.ResUser
+import com.tistory.comfy91.excuseme_android.data.SingletoneToken
+import com.tistory.comfy91.excuseme_android.data.repository.ServerUserDataRepository
+import com.tistory.comfy91.excuseme_android.data.server.BodyChangePw
+import com.tistory.comfy91.excuseme_android.logDebug
+import com.tistory.comfy91.excuseme_android.toast
 import kotlinx.android.synthetic.main.activity_change_password.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class ChangePasswordActivity : AppCompatActivity() {
+    private var userDataRepository = ServerUserDataRepository()
+    private var token = SingletoneToken.getInstance().token
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(com.tistory.comfy91.excuseme_android.R.layout.activity_change_password)
-
-        tvChangepwNotice.setVisibility(View.INVISIBLE)
+        setContentView(R.layout.activity_change_password)
 
         window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE or WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
+        initUi()
+    }
+
+
+    private fun initUi() {
+        tvChangepwNotice.setVisibility(View.INVISIBLE)
 
         et_password_exist.doOnTextChanged { text1, start, count, after ->
-            Log.d("ssss",et_password_new.text.toString())
+            Log.d("ssss", et_password_new.text.toString())
             if (!text1.isNullOrBlank() && !et_password_new.text.toString().isNullOrBlank() && !et_password_new_confirm.text.toString().isNullOrBlank()) {
                 button.background = ContextCompat.getDrawable(this, R.drawable.bg_btn_pink)
+                button.isEnabled = true
 
             } else {
                 button.background = ContextCompat.getDrawable(this, R.drawable.bg_btn_gray)
                 tvChangepwNotice.setVisibility(View.VISIBLE)
+                button.isEnabled = false
             }
         }
         et_password_new.doOnTextChanged { text2, start, count, after ->
             if (!et_password_exist.text.toString().isNullOrBlank() && !text2.isNullOrBlank() && !et_password_new_confirm.text.toString().isNullOrBlank()) {
                 button.background = ContextCompat.getDrawable(this, R.drawable.bg_btn_pink)
                 tvChangepwNotice.setVisibility(View.INVISIBLE)
+                button.isEnabled = true
 
             } else {
                 button.background = ContextCompat.getDrawable(this, R.drawable.bg_btn_gray)
                 tvChangepwNotice.setVisibility(View.VISIBLE)
+                button.isEnabled = false
 
             }
         }
@@ -46,13 +65,57 @@ class ChangePasswordActivity : AppCompatActivity() {
             if (!et_password_exist.text.toString().isNullOrBlank() && !et_password_new.text.toString().isNullOrBlank() && !text3.isNullOrBlank()) {
                 button.background = ContextCompat.getDrawable(this, R.drawable.bg_btn_pink)
                 tvChangepwNotice.setVisibility(View.INVISIBLE)
+                button.isEnabled = true
 
             } else {
                 button.background = ContextCompat.getDrawable(this, R.drawable.bg_btn_gray)
                 tvChangepwNotice.setVisibility(View.VISIBLE)
-
+                button.isEnabled = false
             }
         }
+
+        button.setOnClickListener {
+            if (et_password_new.text != et_password_new_confirm) {
+                "비밀번호가 일치하지 않습니다.".toast(this@ChangePasswordActivity)
+                return@setOnClickListener
+            }
+            changePw()
+        }
+
+
+    }
+
+    private fun changePw() {
+        if (token == null) {
+            token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWR4IjozOSwidXVpZCI6ImYzZDViM2E1LTkwYjYtNDVlMy1hOThhLTEyODE5OWNmZTg1MCIsImlhdCI6MTU3NzkwMTA1MywiZXhwIjoxNTc3OTg3NDUzLCJpc3MiOiJnYW5naGVlIn0.QytUhsXf4bJirRR_zF3wdACiNu9ytwUE4mrPSNLCFLk"
+        }
+
+        userDataRepository.changePw(
+            token!!,
+            BodyChangePw(
+                et_password_exist.text.toString()
+                , et_password_new.text.toString()
+            )
+        ).enqueue(object : Callback<ResUser> {
+            override fun onFailure(call: Call<ResUser>, t: Throwable) {
+                "Fail Request Change Password, message : ${t.message}".logDebug(this@ChangePasswordActivity)
+            }
+
+            override fun onResponse(call: Call<ResUser>, response: Response<ResUser>) {
+                "code: ${response.code()}, message : ${response.message()}".logDebug(this@ChangePasswordActivity)
+                if (response.isSuccessful) {
+                    response.body()
+                        ?.let {
+                            "status : ${it.status}, success : ${it.success}, message : ${it.message}".logDebug(this@ChangePasswordActivity)
+                            finish()
+                        }
+                } else {
+                    "response is Not Success = Body is Empty".logDebug(this@ChangePasswordActivity)
+                }
+            }
+
+        })
+
     }
 }
 
